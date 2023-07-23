8eb18e72eabf6d8d076fa424764a5eef0d1797c6
Iain Lane <iain@orangesquash.org.uk>
2023-07-23 10:44:21 +0100

    Add JWT validation to API handlers
    When we call the API via the gateway after authenticating, it gets sent
    a JWT. Validate this, and check the scopes contain what we asked for.

--- go.mod
+++ go.mod
@@ -5,7 +5,10 @@ go 1.20
 require (
 	github.com/deepmap/oapi-codegen v1.13.0
 	github.com/getkin/kin-openapi v0.118.0
+	github.com/hashicorp/go-retryablehttp v0.7.4
+	github.com/kelseyhightower/envconfig v1.4.0
 	github.com/labstack/echo/v4 v4.11.1
+	github.com/lestrrat-go/jwx/v2 v2.0.11
 	github.com/stretchr/testify v1.8.4
 	github.com/urfave/cli/v2 v2.25.7
 	gorm.io/driver/sqlite v1.5.2
@@ -14,13 +17,21 @@ require (
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.2.0 // indirect
 	github.com/go-openapi/jsonpointer v0.19.5 // indirect
 	github.com/go-openapi/swag v0.21.1 // indirect
+	github.com/goccy/go-json v0.10.2 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
 	github.com/invopop/yaml v0.1.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/labstack/gommon v0.4.0 // indirect
+	github.com/lestrrat-go/blackmagic v1.0.1 // indirect
+	github.com/lestrrat-go/httpcc v1.0.1 // indirect
+	github.com/lestrrat-go/httprc v1.0.4 // indirect
+	github.com/lestrrat-go/iter v1.0.2 // indirect
+	github.com/lestrrat-go/option v1.0.1 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
@@ -28,12 +39,15 @@ require (
 	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826 // indirect
 	github.com/perimeterx/marshmallow v1.1.4 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/segmentio/asm v1.2.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	golang.org/x/crypto v0.11.0 // indirect
+	golang.org/x/mod v0.10.0 // indirect
 	golang.org/x/net v0.12.0 // indirect
 	golang.org/x/text v0.11.0 // indirect
 	golang.org/x/time v0.3.0 // indirect
+	golang.org/x/tools v0.9.2 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/lambda/lambda.go
+++ internal/lambda/lambda.go
@@ -3,7 +3,11 @@ package lambda
 import (
 	"context"
 	"encoding/base64"
+	"encoding/json"
+	"fmt"
 	"os"
+	"strings"
+	"time"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -11,7 +15,12 @@ import (
 	echoadapter "github.com/awslabs/aws-lambda-go-api-proxy/echo"
 	"github.com/deepmap/oapi-codegen/pkg/middleware"
 	"github.com/getkin/kin-openapi/openapi3"
+	"github.com/getkin/kin-openapi/openapi3filter"
+	"github.com/hashicorp/go-retryablehttp"
+	"github.com/kelseyhightower/envconfig"
 	"github.com/labstack/echo/v4"
+	"github.com/lestrrat-go/jwx/v2/jwk"
+	"github.com/lestrrat-go/jwx/v2/jwt"
 	"github.com/sirupsen/logrus"
 	echologrus "github.com/spirosoik/echo-logrus"
 )
@@ -55,6 +64,114 @@ func createLoggingAdapter(e *echoadapter.EchoLambdaV2, logger *logrus.Logger) fu
 	}
 }
 
+type jwksCache struct {
+	*jwk.Cache
+}
+
+// use envconfig to get issuer and client_id
+type oidcConfig struct {
+	Issuer   string `envconfig:"ISSUER" required:"true"`
+	ClientID string `envconfig:"CLIENT_ID" required:"true"`
+}
+
+func authFunc(refreshCtx, reqCtx context.Context, jwksCache *jwksCache, input *openapi3filter.AuthenticationInput) error {
+	var oidcConfig oidcConfig
+	err := envconfig.Process("", &oidcConfig)
+	if err != nil {
+		return fmt.Errorf("failed to load required config from environment: %w", err)
+	}
+	if oidcConfig.Issuer == "" {
+		return fmt.Errorf("required environment variable ISSUER is set to an empty string")
+	}
+	if oidcConfig.ClientID == "" {
+		return fmt.Errorf("required environment variable CLIENT_ID is set to an empty string")
+	}
+
+	headers := input.RequestValidationInput.Request.Header
+
+	//get the jwks from the issuer
+	oidcDiscoveryURL := input.SecurityScheme.OpenIdConnectUrl
+	oidcDiscoveryURL = strings.Replace(oidcDiscoveryURL, "${issuer}", oidcConfig.Issuer, 1)
+
+	// fetch that with a retry using go-retryablehttp
+	retryClient := retryablehttp.NewClient()
+	retryClient.RetryMax = 5
+	retryClient.RetryWaitMin = 1 * time.Second
+	retryClient.RetryWaitMax = 5 * time.Second
+	resp, err := retryClient.Get(oidcDiscoveryURL)
+	if err != nil {
+		return fmt.Errorf("failed to fetch oidc discovery url: %w", err)
+	}
+	defer resp.Body.Close()
+
+	// parse the response
+	var discoveryResp struct {
+		JwksURI string `json:"jwks_uri"`
+	}
+	err = json.NewDecoder(resp.Body).Decode(&discoveryResp)
+	if err != nil {
+		return fmt.Errorf("failed to decode oidc discovery response: %w", err)
+	}
+
+	if !jwksCache.IsRegistered(discoveryResp.JwksURI) {
+		err = jwksCache.Register(discoveryResp.JwksURI)
+		if err != nil {
+			return fmt.Errorf("failed to register URI in jwks cache: %w", err)
+		}
+
+		_, err = jwksCache.Refresh(refreshCtx, discoveryResp.JwksURI)
+		if err != nil {
+			return fmt.Errorf("failed to fetch jwks: %w", err)
+		}
+	}
+
+	keyset, err := jwk.Fetch(reqCtx, discoveryResp.JwksURI)
+	if err != nil {
+		return fmt.Errorf("failed to fetch jwks: %w", err)
+	}
+
+	token, err := jwt.ParseHeader(
+		headers,
+		"Authorization",
+		jwt.WithKeySet(keyset),
+		jwt.WithValidate(true),
+		jwt.WithIssuer(oidcConfig.Issuer),
+		jwt.WithClaimValue("client_id", oidcConfig.ClientID),
+	)
+	if err != nil {
+		return fmt.Errorf("failed to parse jwt: %w", err)
+	}
+
+	if len(input.Scopes) == 0 {
+		return nil
+	}
+
+	scopes, found := token.Get("scope")
+	if !found {
+		wantScopes := strings.Join(input.Scopes, " ")
+		return fmt.Errorf("jwt has no scope claim, required scopes: %s", wantScopes)
+	}
+
+	scopesString, ok := scopes.(string)
+	if !ok {
+		return fmt.Errorf("jwt scope claim is not a string")
+	}
+
+	scopeSet := make(map[string]struct{})
+	for _, scope := range strings.Split(scopesString, " ") {
+		scopeSet[scope] = struct{}{}
+	}
+
+	// finally check each of the `input.Scopes` are in the jwt
+	for _, scope := range input.Scopes {
+		if _, ok := scopeSet[scope]; !ok {
+			return fmt.Errorf("jwt does not have required scope: %s", scope)
+		}
+	}
+
+	return nil
+}
+
 func StartServer[T any](
 	getSwaggerFunc func() (*openapi3.T, error),
 	registerHandlersFunc func(router EchoRouter, handler T),
@@ -72,14 +189,29 @@ func StartServer[T any](
 		logger.Fatalf("Error loading swagger spec\n: %s", err)
 	}
 
+	serverCtx := context.Background()
+	serverCtx, cancel := context.WithCancel(serverCtx)
+	defer cancel()
+
+	jwksCache := &jwksCache{
+		Cache: jwk.NewCache(serverCtx),
+	}
+
 	// swagger.Server = nil
+	options := &middleware.Options{
+		Options: openapi3filter.Options{
+			AuthenticationFunc: func(reqCtx context.Context, input *openapi3filter.AuthenticationInput) error {
+				return authFunc(serverCtx, reqCtx, jwksCache, input)
+			},
+		},
+	}
 
 	e := echo.New()
 	e.HideBanner = true
 	e.HidePort = true
 	e.Logger = loggerMiddleware
 	e.Use(loggerMiddleware.Hook())
-	e.Use(middleware.OapiRequestValidator(swagger))
+	e.Use(middleware.OapiRequestValidatorWithOptions(swagger, options))
 
 	registerHandlersFunc(e, handler)
 
